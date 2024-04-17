77e7d334055ac39bbbf4300341b784eba8421834
AshutoshSinghJai <ashutoshsingh.jai@gmail.com>
2024-04-17 05:35:52 +0530

    first java code uplod on github

---
+++ code.java
@@ -0,0 +1,10 @@
+class test  
+    {  
+    public static void main(String args[])   
+    {  
+    int n1 = 225, n2 = 116, sum;  
+    sum = n1 + n2;  
+    System.out.println("The sum of numbers is: "+sum);  
+    }  
+    
+}
